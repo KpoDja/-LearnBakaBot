2911e87c665edffe8ee7ad179d163838c0325cd6
Yury Larin <Djamtter@gmail.com>
2023-02-19 13:54:29 +0300

    bot_newCommandHandler

--- tg_bot.py
+++ tg_bot.py
@@ -5,7 +5,6 @@ import settings
 
 logging.basicConfig(filename="bot.log", level=logging.INFO)
 
-
 all_planet = {
     "Марс": ephem.Mars(datetime.date.today()), "Венера": ephem.Venus(datetime.date.today()),
     "Меркурий": ephem.Mercury(datetime.date.today()),
@@ -24,15 +23,28 @@ def planet_info(update, context):
     constellation = ephem.constellation(all_planet[text.capitalize()])
 
     if text.capitalize() in all_planet:
-        print(update.message.reply_text(constellation))
+        update.message.reply_text(constellation)
     else:
-        print("Не наблюдается")
+        update.message.reply_text("Не наблюдается")
 
 
 def talk_to_me(update, context):
     text = update.message.text
-    print(text)
     update.message.reply_text(text)
+
+
+def wordcount(update, context):
+    word_count = update.message.text.split(" ")[1:]
+    update.message.reply_text(f"количество слов {len(word_count)}")
+
+
+def moon(update, context):
+    moon = ephem.next_full_moon(datetime.date.today())
+    moon_date = update.message.text.split(" ")[1]
+    if moon_date.capitalize() == "Затмение":
+        update.message.reply_text(moon)
+    else:
+        update.message.reply_text("Когда же?")
 
 
 def main():
@@ -41,6 +53,8 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("planet", planet_info))
+    dp.add_handler(CommandHandler("wordcount", wordcount))
+    dp.add_handler(CommandHandler("moon", moon))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info("Бот стартовал")
